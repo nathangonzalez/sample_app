140b4c84e6d9a32bd6f1b009afb3b85aba784cca
Nathan Gonzalez <nathangonzalez12@gmail.com>
2016-07-13 00:21:34 +0000

    test

--- toy_app/app/controllers/application_controller.rb
+++ toy_app/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
+  def index
       render html: "Jayden & Caleb...Daddy is in the WWW"
     end
 end
