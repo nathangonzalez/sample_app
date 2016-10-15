327315d14d3224c66b37d5089bf621e7e4e68c6b
Nathan Gonzalez <nathangonzalez12@gmail.com>
2016-10-15 18:49:24 +0000

    test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,12 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+module ApplicationHelper
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
