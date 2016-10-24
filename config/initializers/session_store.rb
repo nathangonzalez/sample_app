f7eff76fcadec3466493f43e0fdb4e1f8e1828ee
Nathan Gonzalez <nathangonzalez12@gmail.com>
2016-10-24 03:20:42 +0000

    test

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
-Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
-=======
 Rails.application.config.session_store :cookie_store, key: '_sample_app__session'
->>>>>>> 3aac1ac9316816b0f57c603200605ff00eb42da5
