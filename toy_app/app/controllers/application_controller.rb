b3415d08caea29140aceea0be16ca168929ee086
Nathan Gonzalez <nathangonzalez12@gmail.com>
2016-11-11 02:05:27 +0000

    test

--- toy_app/app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  
-  def index
-      render html: "Jayden & Caleb...Daddy is in the WWW"
-    end
-end
--- toy_app/app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  belongs_to :user  
-  validates :content, lenght: {maximum: 140}
-end
